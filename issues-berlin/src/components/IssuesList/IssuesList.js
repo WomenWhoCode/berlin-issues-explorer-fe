05e2cbab1ebf42dc9ba6ada36ebbb5bf50b914ab
Nuno Gervasio <nunogee@gmail.com>
2019-04-15 09:28:40 +0200

    add snapshot test and component code

--- issues-berlin/src/components/IssuesList/IssuesList.js
+++ issues-berlin/src/components/IssuesList/IssuesList.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import './IssuesList.css';
+import React from "react";
+import PropTypes from "prop-types";
+import Issue from "../Issue/Issue";
+import "./IssuesList.css";
 
 class IssuesList extends React.Component {
   render() {
+    const { issues } = this.props;
+
+    if (issues.length === 0) return `There are no biginner friendly issues`;
+
+    const issuesList = issues.map((issue, index) => (
+      <Issue
+        key={index}
+        title={issue.title}
+        labels={issue.labels}
+        repositoryTitle={issue.repository_title}
+        repositoryLabels={issue.repository_labels}
+        repositoryURL={issue.repository_url}
+        issueLanguage={issue.language}
+      />
+    ));
     return (
-      <h3 className="issueslist-title">Beginner Friendly Issues:</h3>
-    )
+      <React.Fragment>
+        <h3 className="issueslist-title">Beginner Friendly Issues:</h3>
+        <ul>{issuesList}</ul>
+      </React.Fragment>
+    );
   }
 }
 
 export default IssuesList;
+
+IssuesList.propTypes = {
+  issues: PropTypes.array.isRequired
+};
